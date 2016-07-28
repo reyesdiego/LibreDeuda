5ef9f90f35dbf807fc6a3a13497a6a1737b41afe
Diego Reyes <reyesdiego@hotmail.com>
2016-07-28 09:05:28 -0300

    Refactors. Se agrega funcinalidad Lugar, Forwarding

--- Server/routes/lde.js
+++ Server/routes/lde.js
@@ -8,29 +8,10 @@ module.exports = (socket) => {
     var router = express.Router();
     var FreeDebt = require("../models/freeDebt.js");
     var moment = require("moment");
-
-    router.get("/", (req, res) => {
-        var result;
-
-        var free = FreeDebt.find();
-        free.exec((err, data) => {
-            if (err) {
-                result = {
-                    status: "ERROR",
-                    message: err.message,
-                    data: err};
-                res.status(500).send(result);
-            } else {
-                result = {
-                    status: "OK",
-                    data: data};
-                res.status(200).send(result);
-            }
-        });
-    });
+    var Lde = require('../lib/lde.js');
+    Lde = new Lde();
 
     let getFreeDebt = (req, res) => {
-
         if (req.url.indexOf('/lugar') >= 0) {
             let place = require('../lib/place.js');
             let ID = req.query.ID;
@@ -53,11 +34,8 @@ module.exports = (socket) => {
                         res.status(500).send(err);
                     });
             }
-
         } else {
-            let lde = require('../lib/lde.js');
-            lde = new lde();
-            lde.getLde(req.params)
+            Lde.checkLde(req.params)
                 .then(data => {
                     res.status(200).send(data);
                 })
@@ -68,73 +46,80 @@ module.exports = (socket) => {
 
     };
 
-    let putFreeDebt = (req, res) => {
-        const DISABLED = 9;
-        const ENABLED = 0;
-        const INVOICED = 3;
-        var contenedor = req.body.CONTENEDOR;
-        var user = req.user;
-        var status = -1;
-        var description = '';
-        var newStatus = {
-            AUD_USER: user.USUARIO,
-            AUD_TIME: Date.now()
+    let enableFreeDebt = (req, res) => {
+        var param = {
+            contenedor: req.body.CONTENEDOR,
+            user: req.user
         };
 
-        FreeDebt.findOne({CONTAINER: contenedor})
-        .exec((err, data) => {
-            if (err) {
-                res.status(500).send({status: "ERROR", message: err.message, data: err});
+        Lde.enableLde(param)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    };
+
+    let disableFreeDebt = (req, res) => {
+
+        var param = {
+            contenedor: req.body.CONTENEDOR,
+            user: req.user
+        };
+
+        Lde.disableLde(param)
+        .then(data => {
+                res.status(200).send(data);
+            })
+        .catch(err => {
+                res.status(500).send(err);
+            });
+    };
+
+    let forwardLde = (req, res) => {
+        var Cuit = require("../include/cuit.js");
+        var moment = require("moment");
+
+        var checkCuit = Cuit(req.body.CUIT);
+        if (checkCuit === false) {
+            res.status(400).send({status: "ERROR", message: "El CUIT es inválido", data: {CUIT: req.body.CUIT}});
+        } else {
+            let fecha = moment(req.body.FECHA_DEV, "YYYY-MM-DD").toDate();
+            if (fecha < new Date()) {
+                res.status(400).send({status: "ERROR", message: "La Fecha de Devolución debe ser mayor a la fecha actual", data: {FECHA_DEV: fecha}});
             } else {
-                if (!data) {
-                    res.status(403).send({status: "ERROR", message: "No Existe el Libre Deuda para el Contenedor."});
-                } else {
-                    var lastStatus = data.STATUS[data.STATUS.length-1];
-
-                    if (req.route.path.indexOf('/disable') >= 0) {
-                        if (lastStatus.STATUS === DISABLED) {
-                            description = 'Deshabilitado';
-                        } else if (lastStatus.STATUS === INVOICED) {
-                            description = 'Facturado';
-                        } else {
-                            status = DISABLED;
-                        }
-                    } else if (req.route.path.indexOf('/enable')  >= 0) {
-                        if (lastStatus.STATUS === ENABLED) {
-                            description = 'Habilitado';
-                        } else if (lastStatus.STATUS === INVOICED) {
-                            description = 'Facturado';
-                        } else {
-                            status = ENABLED;
-                        }
-                    } else if (req.route.path.indexOf('/invoice')  >= 0) {
-                        if (lastStatus.STATUS === INVOICED) {
-                            description = 'Facturado';
-                        } else if (lastStatus.STATUS === DISABLED) {
-                            description = 'Deshabilitado';
-                        } else {
-                            status = INVOICED;
-                        }
-                    }
-
-                    if (description !== '') {
-                        res.status(403).send({
-                            status: "ERROR",
-                            message: `El Libre Deuda del Contenedor yá se encuentra ${description}.`
-                        });
-                    } else {
-                        newStatus.STATUS = status;
-                        data.STATUS.push(newStatus);
-                        data.save((err, rowAffected) => {
-                            res.status(200).send({
-                                status: "OK",
-                                data: data
-                            });
-                        });
-                    }
-                }
+                var param = {
+                    user: req.user,
+                    contenedor: req.body.CONTENEDOR,
+                    cuit: req.body.CUIT,
+                    fecha_dev: moment(req.body.FECHA_DEV, "YYYY-MM-DD").toDate()
+                };
+                Lde.forwardLde(param)
+                    .then(data => {
+                        res.status(200).send(data);
+                    })
+                    .catch(err => {
+                        res.status(500).send(err);
+                    });
             }
-        });
+        }
+    };
+
+    let invoiceFreeDebt = (req, res) => {
+        var param = {
+            contenedor: req.body.CONTENEDOR,
+            email: req.body.EMAIL_CLIENTE,
+            user: req.user
+        };
+
+        Lde.invoiceLde(param)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
     };
 
     let addFreeDebt = (req, res) => {
@@ -145,10 +130,7 @@ module.exports = (socket) => {
         var timestamp = moment().toDate();
         var lde2insert;
 
-        var ldeClass = require('../lib/lde.js');
-
-        ldeClass = new ldeClass();
-        ldeClass.getLde({contenedor: lde.CONTENEDOR})
+        Lde.checkLde({contenedor: lde.CONTENEDOR})
             .then(data => {
                 let result = {
                     status: "ERROR",
@@ -214,11 +196,33 @@ module.exports = (socket) => {
             });
     };
 
+    let changePlace = (req, res) => {
+
+        var contenedor = req.body.CONTENEDOR;
+        var id_cliente = req.body.ID_CLIENTE;
+
+        var param = {
+            contenedor: contenedor,
+            id_cliente: id_cliente,
+            lugar_dev: req.body.LUGAR_DEV
+        };
+
+        Lde.checkLde(param)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    };
+
     router.get("/:contenedor", getFreeDebt);
-    router.put("/disable", putFreeDebt);
-    router.put("/enable", putFreeDebt);
-    router.put("/invoice", putFreeDebt);
+    router.put("/disable", disableFreeDebt);
+    router.put("/enable", enableFreeDebt);
+    router.put("/invoice", invoiceFreeDebt);
     router.post("/", addFreeDebt);
+    router.put("/lugar", changePlace);
+    router.put("/forward", forwardLde);
 
     return router;
-}
+};
