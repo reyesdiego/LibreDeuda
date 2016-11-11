710be2397112613ae9e1fbc621f1b09743a5f8b4
Diego Reyes <reyesdiego@hotmail.com>
2016-11-11 16:41:44 -0300

    Implemento modulo para verificar Content-Type

---
+++ Server/include/contentType.js
@@ -0,0 +1,35 @@
+/**
+ * Created by diego on 11/11/16.
+ */
+
+var error = require('./error.js');
+
+var isApplicationJson = (req, res, next) => {
+    var APPLICATION_JSON = "application/json";
+    var contentType = req.headers["content-type"].toLowerCase();
+    if (APPLICATION_JSON === contentType) {
+        next();
+    } else {
+        var result = {
+            status: "ERROR",
+            message: "El Content-Type debe ser application/json"
+        }
+        res.status(415).send(result);
+    }
+};
+module.exports.isApplicationJson = isApplicationJson;
+
+var isTextPlain = (req, res, next) => {
+    var TEXT_PLAIN = "text/plain";
+    var contentType = req.headers["content_type"].toLowerCase();
+    if (TEXT_PLAIN === contentType) {
+        next();
+    } else {
+        var result = {
+            status: "ERROR",
+            message: "El Content-Type debe ser text/plain"
+        }
+        res.status(415).send(result);
+    }
+};
+module.exports.isTextPlain = isTextPlain;
