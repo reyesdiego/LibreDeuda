a4ff05f7bd5973e83914019dff2f953dbc7e547c
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-12-13 11:15:46 -0300

    Arreglos visuales

--- Client/register/register.controller.js
+++ Client/register/register.controller.js
@@ -1,27 +1,12 @@
 /**
  * Created by kolesnikov-a on 28/10/2016.
  */
-myApp.controller('registerCtrl', ['$scope', 'configService', 'Register', 'dialogsService', 'validatorService', function($scope, configService, Register, dialogsService, validatorService){
+myApp.controller('registerCtrl', ['$scope', 'Register', 'dialogsService', 'validatorService', function($scope, Register, dialogsService, validatorService){
 
 	$scope.user = Register;
 
-	/*$scope.terminales = {
-		BACTSSA: false,
-		TERMINAL4: false,
-		TRP: false
-	};*/
-
 	$scope.confirmPassword = '';
 	$scope.validator = validatorService;
-
-	/*$scope.getTerminals = function(){
-		let terminalsArray = [];
-		for (let terminal in $scope.terminales){
-			let value = $scope.terminales[terminal];
-			if (value) terminalsArray.push(value);
-		}
-		$scope.user.data.terminals = terminalsArray;
-	};*/
 
 	$scope.send = function(){
 			if (!$scope.validator.validateCuit($scope.user.data.cuit)){
