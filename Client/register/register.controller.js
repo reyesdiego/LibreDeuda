32383bbe61abe833d8212fd2547b13f430e5b56c
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-12-13 16:37:51 -0300

    Arreglos y mejoras en registro de usuarios

--- Client/register/register.controller.js
+++ Client/register/register.controller.js
@@ -9,17 +9,17 @@ myApp.controller('registerCtrl', ['$scope', 'Register', 'dialogsService', 'valid
 	$scope.validator = validatorService;
 
 	$scope.send = function(){
-			if (!$scope.validator.validateCuit($scope.user.data.cuit)){
+			if (!$scope.validator.validateCuit($scope.user.cuit)){
 				dialogsService.notify('Registro', 'El CUIT ingresado no es válido.');
-			} else if ($scope.user.data.password != $scope.confirmPassword){
+			} else if ($scope.user.password != $scope.confirmPassword){
 				dialogsService.notify('Registro', 'Las contraseñas ingresadas no coinciden.');
-				$scope.user.data.password = '';
+				$scope.user.password = '';
 				$scope.confirmPassword = '';
 			} else {
 				//console.log($scope.user);
 				$scope.user.register().then((data) => {
 					//console.log(data);
-					dialogsService.notify('Registro', `Se ha enviado un mail a la cuenta de correo ${$scope.user.data.email}, ingrese para validar su usuario.`);
+					dialogsService.notify('Registro', `Se ha enviado un mail a la cuenta de correo ${$scope.user.email}, ingrese para validar su usuario.`);
 				}, (error) => {
 					//console.log(error);
 					dialogsService.error('Registro', 'Se produjo un error al intentar crear el nuevo usuario.');
--- Client/services/register.service.js
+++ Client/services/register.service.js
@@ -3,33 +3,91 @@
  */
 myApp.service('Register', ['configService', '$http', '$q', function(configService, $http, $q){
 
-	this.data = {
-		group: 'AGE',
-		company: '',
-		cuit: '',
-		emailContact: '',
-		telephone: '',
-		//terminals: [],
-		firstname: '',
-		lastname: '',
-		position: '',
-		email: '',
-		password: ''
-	};
+	class Register {
+		constructor(){
+			this.data = {
+				group: 'AGE',
+				company: '',
+				cuit: '',
+				emailContact: '',
+				telephone: '',
+				//terminals: [],
+				firstname: '',
+				lastname: '',
+				position: '',
+				email: '',
+				password: ''
+			};
+		}
 
-	this.register = function(){
-		let deferred = $q.defer();
-		let inserturl = `${configService.serverUrl}/register`;
-		console.log(this.data);
-		$http.post(inserturl, this.data).then((response) => {
-			console.log(response);
-			deferred.resolve(response.data);
-		}, (response) => {
-			console.log(response);
-			deferred.reject(response.data);
-			//deferred.resolve(this.data); //solo para probar
-		});
-		return deferred.promise;
+		register(){
+			let deferred = $q.defer();
+			let inserturl = `${configService.serverUrl}/register`;
+			//console.log(this.data);
+			$http.post(inserturl, this.data).then((response) => {
+				//console.log(response);
+				deferred.resolve(response.data);
+			}, (response) => {
+				//console.log(response);
+				deferred.reject(response.data);
+				//deferred.resolve(this.data); //solo para probar
+			});
+			return deferred.promise;
+		}
+
+		set group(data){
+			this.data.group = data;
+		}
+
+		set company(data){
+			this.data.company = data;
+		}
+
+		set cuit(data){
+			this.data.cuit = data;
+		}
+
+		get cuit(){
+			return this.data.cuit;
+		}
+
+		set emailContact(data){
+			this.data.emailContact = data;
+		}
+
+		set telephone(data){
+			this.data.telephone = data;
+		}
+
+		set firstName(data){
+			this.data.firstname = data;
+		}
+
+		set lastName(data){
+			this.data.lastname = data;
+		}
+
+		set position(data){
+			this.data.position = data;
+		}
+
+		set email(data){
+			this.data.email = data;
+		}
+
+		get email(){
+			return this.data.email;
+		}
+
+		set password(data){
+			this.data.password = data;
+		}
+
+		get password(){
+			return this.data.password;
+		}
 	}
+
+	return new Register();
 
 }]);
