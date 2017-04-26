3574d2c806121640486d2a3b36a6116286014de1
Diego Reyes <reyesdiego@hotmail.com>
2017-04-26 16:13:16 -0300

    Bug fixing con fecha devolucion en mail de insert lde. Cambio de logo en mails

--- Server/include/micro-emailjs.js
+++ Server/include/micro-emailjs.js
@@ -7,14 +7,34 @@ const config = require("../config/config.js");
 var seneca = require("seneca")();
 seneca.client( config.microService.email.port, config.microService.email.host);
 
+function promisify(err, data) {
+    return new Promise((resolve, reject) => {
+        if (err) {
+            reject(err);
+        } else {
+            if (data.status === 'OK') {
+                resolve({status: "OK",
+                    data: data.data});
+            } else {
+                reject({status: "ERROR",
+                    message: data.message,
+                    data: data.data});
+            }
+        }
+    });
+}
+
+
 const send = (to, subject, text, attachment) => {
     return new Promise((resolve, reject) => {
         seneca.act({role: "email", cmd: "send", to: to, subject: subject, text: text}, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
+            promisify(err, data)
+            .then(data => {
+                    resolve(data);
+                })
+            .catch(err => {
+                    reject(err);
+                });
         });
     });
 };
