1154461b259159897f853a7f08409b6e735cba4a
Diego Reyes <reyesdiego@hotmail.com>
2017-11-28 14:34:11 -0300

    Agrego testing con contenedor.js include

--- Server/Test/testCuit.js
+++ Server/Test/testCuit.js
@@ -1,12 +1,20 @@
 /**
  * Created by diego on 16/12/16.
  */
+//@ts-check
 
-
+"use strict";
 
 var cuit = "30688415531";
 
 var Cuit = require("../include/cuit.js");
 
 console.log(Cuit(cuit));
+
+
+var contenedor = "CSQU3054383";
+
+var Contenedor = require("../include/container.js");
+
+console.log(Contenedor(contenedor)); 
 
