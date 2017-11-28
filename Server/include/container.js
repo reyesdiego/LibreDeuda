53bb14a44c3980e07a014a977bfaa7ea3603b9c8
Diego Reyes <reyesdiego@hotmail.com>
2017-11-28 14:33:47 -0300

    eslint tscheck tyding

--- Server/include/container.js
+++ Server/include/container.js
@@ -3,19 +3,26 @@
  * http://en.wikipedia.org/wiki/ISO_6346
  * for testing use : CSQU3054383
  */
-'use strict';
+//@ts-check
 
-let check = (container) => {
+"use strict";
+/**
+ * Este módulo chequea el dígito verificador del número de Contenedor
+ * 
+ * @param {String} container 
+ * @returns 
+ */
+const check = (container) => {
     var response = false;
-    var letters = {"A": 10, "B": 12, "C": 13, "D": 14, "E": 15, "F": 16, "G": 17, "H": 18, "I": 19, "J": 20, "K": 21, "L": 23, "M": 24, "N": 25, "O": 26, "P": 27, "Q": 28, "R": 29, "S": 30, "T": 31, "U": 32, "V": 34, "W": 35, "X": 36, "Y": 37, "Z": 38};
-    var step1=0;
+    var letters = { A: 10, B: 12, C: 13, D: 14, E: 15, F: 16, G: 17, H: 18, I: 19, J: 20, K: 21, L: 23, M: 24, N: 25, O: 26, P: 27, Q: 28, R: 29, S: 30, T: 31, U: 32, V: 34, W: 35, X: 36, Y: 37, Z: 38 };
+    var step1 = 0;
 
     if (container !== undefined) {
-        for (var i = 0; i<4; i++) {
+        for (var i = 0; i < 4; i++) {
             step1 += (Math.pow(2, i) * parseInt(letters[container[i]], 10));
         }
 
-        for (i = 4; i<10; i++) {
+        for (i = 4; i < 10; i++) {
             step1 += (Math.pow(2, i) * parseInt(container[i], 10));
         }
 
@@ -27,6 +34,6 @@ let check = (container) => {
     }
 
     return response;
-}
+};
 
 module.exports = check;
