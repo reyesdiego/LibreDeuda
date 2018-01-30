5ba38c36949f8aba28b508cb3d7e9d8809251db6
Diego Reyes <reyesdiego@hotmail.com>
2018-01-30 14:35:28 -0300

    tyding code eslint

--- Server/include/error.js
+++ Server/include/error.js
@@ -2,24 +2,24 @@
  * Created by diego on 10/11/16.
  */
 
-'use strict';
+"use strict";
 
 var errors = [
-    {code: `MONGO-ERROR`, message: `Error en base de datos`, http_status: 500},
-    {code: `AGP-0001`, message: `Libre deuda no existe para este contenedor.`, http_status: 409},
-    {code: `AGP-0002`, message: `Libre deuda vencido para este contenedor.`, http_status: 409},
-    {code: `AGP-0003`, message: `Libre deuda anulado para este contenedor.`, http_status: 409},
-    {code: `AGP-0004`, message: `CUIT inválido.`, http_status: 400},
-    {code: `AGP-0005`, message: `Libre deuda yá existe para este contenedor.`, http_status: 409},
-    {code: `AGP-0006`, message: `La fecha no puede ser menor a la fecha de hoy.`, http_status: 409},
-    {code: `AGP-0007`, message: `La nueva fecha de devolución debe ser menor a la fecha de devolución vigente.`, http_status: 409},
-    {code: `AGP-0008`, message: `No tiene privilegios para realizar esta petición.`, http_status: 401},
-    {code: `AGP-0009`, message: `Token inválido`, http_status: 401},
-    {code: `AGP-0010`, message: `El usuario no existe`, http_status: 401},
-    {code: `AGP-0011`, message: `La clave es incorrecta`, http_status: 401},
-    {code: `AGP-0012`, message: `Debe proveer un Usuario`, http_status: 403},
-    {code: `AGP-0013`, message: `Lo parámetros de entrada son incorrectos`, http_status: 403},
-    {code: `AGP-0014`, message: `No tiene privilegios para asignar un nuevo CUIT al Libre Deuda Electrónico`, http_status: 401}
+    {code: "MONGO-ERROR", message: "Error en base de datos", http_status: 500},
+    {code: "AGP-0001", message: "Libre deuda no existe para este contenedor.", http_status: 409},
+    {code: "AGP-0002", message: "Libre deuda vencido para este contenedor.", http_status: 409},
+    {code: "AGP-0003", message: "Libre deuda anulado para este contenedor.", http_status: 409},
+    {code: "AGP-0004", message: "CUIT inválido.", http_status: 400},
+    {code: "AGP-0005", message: "Libre deuda yá existe para este contenedor.", http_status: 409},
+    {code: "AGP-0006", message: "La fecha no puede ser menor a la fecha de hoy.", http_status: 409},
+    {code: "AGP-0007", message: "La nueva fecha de devolución debe ser menor a la fecha de devolución vigente.", http_status: 409},
+    {code: "AGP-0008", message: "No tiene privilegios para realizar esta petición.", http_status: 401},
+    {code: "AGP-0009", message: "Token inválido", http_status: 401},
+    {code: "AGP-0010", message: "El usuario no existe", http_status: 401},
+    {code: "AGP-0011", message: "La clave es incorrecta", http_status: 401},
+    {code: "AGP-0012", message: "Debe proveer un Usuario", http_status: 403},
+    {code: "AGP-0013", message: "Lo parámetros de entrada son incorrectos", http_status: 403},
+    {code: "AGP-0014", message: "No tiene privilegios para asignar un nuevo CUIT al Libre Deuda Electrónico", http_status: 401}
 ];
 //module.exports.errors = errors;
 
@@ -42,9 +42,9 @@ module.exports.getErrors = getError;
 
 var ERROR = code => {
     let err = getError(code);
-    this.status = 'ERROR';
-    this.code = '';
-    this.message = '';
+    this.status = "ERROR";
+    this.code = "";
+    this.message = "";
     this.http_status = 500;
 
     if (err) {
