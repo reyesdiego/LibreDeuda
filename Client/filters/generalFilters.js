e066879aab7555015ef7b9496bb8246bbf312e79
Diego Reyes <reyesdiego@hotmail.com>
2018-02-01 12:01:37 -0300

    Reconfigurando Folders

--- Client/filters/generalFilters.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kolesnikov-a on 21/04/2016.
- */
-myApp.filter('startFrom', [function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
-    }
-}]);
